48cb7d169750555e6766af1495bf3b1ead6ac140
Farvez Hossen <56191538+Farvez999@users.noreply.github.com>
2023-11-11 19:19:17 +0600

    Category Added

--- lib/widget/CategoryWidget.dart
+++ lib/widget/CategoryWidget.dart
@@ -5,35 +5,54 @@ class CategoryWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: Row(
-        children: [
-          Container(
-            margin: EdgeInsets.symmetric(horizontal : 10),
-            padding: EdgeInsets.symmetric(vertical : 5, horizontal: 10),
-            height: 50,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Image.asset("images/bag.jpg", width: 40, height: 40,),
-                Text(
-                  "Bag",
-                  style: TextStyle(
+
+    return SizedBox(
+      height: 200,
+      child: ListView.separated(
+          physics: BouncingScrollPhysics(),
+          scrollDirection: Axis.horizontal,
+          shrinkWrap: true,
+          itemBuilder: (context,index){
+        return Container(
+          margin: EdgeInsets.symmetric(horizontal : 10),
+          padding: EdgeInsets.symmetric(vertical : 5, horizontal: 10),
+          height: 50,
+          width: 200,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset("images/bag.jpg", width: 40, height: 40,),
+              Text(
+                "Bag",
+                style: TextStyle(
                     fontWeight: FontWeight.bold,
-                    fontSize: 16,
+                    fontSize: 17,
                     color: Color(0xFF4C53A5)
-                  ),
-                )
-              ],
-            ),
-          )
-        ],
-      ),
+                ),
+              )
+            ],
+          ),
+        );
+      }, separatorBuilder: (context,index){
+
+
+            if(index==2){
+              return Container(
+                height: 10,
+                width: 10,
+                color: Colors.teal,
+              );
+            }else{
+              return SizedBox(width: 5,);
+            }
+
+
+
+      }, itemCount: 8),
     );
   }
 }
